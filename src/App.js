721c034ce2169ee32a9b82e24e385c2ba35d282f
Muhammad Idrees <idreeeeees1999@gmail.com>
2022-09-06 11:29:48 +0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AsyncSum from "./components/AsyncSum";
 import Fetch from "./components/Fetch";
+import EnhancedTable from "./components/SortingTable-MaterialUI";
 
 const App = () => {
 	return (
 		<>
 			<AsyncSum />
 			<Fetch />
+			<EnhancedTable />
 		</>
 	);
 };
